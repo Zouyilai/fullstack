99884197b28621d57c524b964a83637a78ac47ad
ZOU YI LAI <zouyilai@gmail.com>
2023-05-05 19:17:09 -0300

    change token

--- trabalho02/routes/premio-route.js
+++ trabalho02/routes/premio-route.js
@@ -16,50 +16,94 @@ router.post('/premio',
     body('quantidade').isNumeric().withMessage("Quantidade deve ser número!"),
     async (req, res) => {
         const authHeader = req.headers["authorization"];
-        const token = authHeader.split(" ")[1]
-        var decodificado;
-        try {
-            decodificado = jsonwebtoken.verify(token, "topsecret");
-            console.log(decodificado);
-        } catch (err) {
-            res.status(401).json({resultado: 'Token inválido!'})
-            return;
-        }
+        if(authHeader){
+            const token = authHeader.split(" ")[1]
+            var decodificado;
+            try {
+                decodificado = jsonwebtoken.verify(token, "topsecret");
+                console.log(decodificado);
+            } catch (err) {
+                res.status(401).json({resultado: 'Token inválido!'})
+                return;
+            }
 
-        const validacao = validationResult(req).array();
-        if (validacao.length === 0) {
-            const novo = await premioController.criarPremio(req.body.descricao, 
-                                                            req.body.pontos,
-                                                            req.body.quantidade);
-            res.status(200).json({resultado: 'Prêmio criado!', premio: novo});
+            const validacao = validationResult(req).array();
+            if (validacao.length === 0) {
+                const novo = await premioController.criarPremio(req.body.descricao, 
+                                                                req.body.pontos,
+                                                                req.body.quantidade);
+                res.status(200).json({resultado: 'Prêmio criado!', premio: novo});
+            } else {
+                res.status(400).json(validacao);
+            }
         } else {
-            res.status(400).json(validacao);
+            res.status(400).json({resultado: 'Informe o token!'});
         }
 })
 
 // /premio/{id} (GET): obtém um prêmio pelo id
 router.get('/premio/:id', async (req, res) => {
     const authHeader = req.headers["authorization"];
-    const token = authHeader.split(" ")[1]
-    var decodificado;
-    try {
-        decodificado = jsonwebtoken.verify(token, "topsecret");
-        // console.log(decodificado);
-    } catch (err) {
-        res.status(401).json({resultado: 'Token inválido!'})
-        return;
-    }
+    if(authHeader){
+        const token = authHeader.split(" ")[1]
+        var decodificado;
+        try {
+            decodificado = jsonwebtoken.verify(token, "topsecret");
+            // console.log(decodificado);
+        } catch (err) {
+            res.status(401).json({resultado: 'Token inválido!'});
+            return;
+        }
 
-    const idPremio = req.params.id;
-    const resultado = await premioController.visualizarPremio(idPremio);
-    if(resultado) {
-        res.status(200).json({resultado: resultado});
+        const idPremio = req.params.id;
+        const resultado = await premioController.visualizarPremio(idPremio);
+        if(resultado) {
+            res.status(200).json({resultado: resultado});
+        } else {
+            res.status(404).json({resultado: "Premio não encontrado!"});
+        }
     } else {
-        res.status(404).json({resultado: "Premio não encontrado!"});
+        res.status(400).json({resultado: 'Informe o token!'});
     }
 })
 
-// /usuario/{id} (PUT): atualiza um usuário pelo id
+// /premio/{id} (PUT): atualizar um prêmio pelo id
+router.put('/premio/:id', 
+    body('quantidade').notEmpty().withMessage("Quantidade nova inválida!"),
+    body('quantidade').isNumeric().withMessage("Quantidade nova deve ser número!"),
+    async (req, res) => {
+        const authHeader = req.headers["authorization"];
+        if(authHeader){
+            const token = authHeader.split(" ")[1]
+            var decodificado;
+            try {
+                decodificado = jsonwebtoken.verify(token, "topsecret");
+            } catch (err) {
+                res.status(401).json({resultado: 'Token inválido!'});
+                return;
+            }
+
+            const validacao = validationResult(req).array();
+            if (validacao.length === 0) {
+                const idPremio = req.params.id;
+                const quantidade = req.body.quantidade;
+                const resultado = await premioController.atualizarPremio(idPremio, quantidade);
+                if (resultado){
+                    res.status(200).json({resultado: "Prêmio atualizado com sucesso!"});
+                } else {
+                    res.status(404).json({resultado: 'Prêmio não encontrado!'});
+                }
+            } else {
+                res.status(400).json(validacao);
+            }
+        } else {
+            res.status(400).json({resultado: 'Informe o token!'});
+        }
+})
+
+
+// /premio/{id} (DELETE): remover um prêmio pelo id
+
 
 module.exports = router;
 
